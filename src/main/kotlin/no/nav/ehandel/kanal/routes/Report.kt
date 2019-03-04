57bf441afe59971129b28a19701bb589b2a64986
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2019-03-04 16:18:53 +0100

    refactor: remove unused route path for report

--- src/main/kotlin/no/nav/ehandel/kanal/routes/Report.kt
+++ src/main/kotlin/no/nav/ehandel/kanal/routes/Report.kt
@@ -6,13 +6,10 @@ import io.ktor.http.ContentDisposition
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
-import io.ktor.request.receive
 import io.ktor.response.header
-import io.ktor.response.respond
 import io.ktor.response.respondBytes
 import io.ktor.routing.Routing
 import io.ktor.routing.get
-import io.ktor.routing.post
 import io.ktor.routing.route
 import kotlinx.html.a
 import kotlinx.html.body
@@ -22,7 +19,6 @@ import kotlinx.html.li
 import kotlinx.html.title
 import kotlinx.html.ul
 import no.nav.ehandel.kanal.formatDate
-import no.nav.ehandel.kanal.report.CsvValues
 import no.nav.ehandel.kanal.report.Report
 import org.joda.time.DateTime
 
@@ -54,10 +50,6 @@ fun Routing.report() {
             val fileName = call.parameters["fileName"] ?: throw IllegalArgumentException("Missing parameter 'fileName'")
             call.response.header(HttpHeaders.ContentDisposition, ContentDisposition.Attachment.withParameter(ContentDisposition.Parameters.FileName, "$fileName.csv").toString())
             call.respondBytes(contentType = ContentType.Text.CSV, status = HttpStatusCode.OK, bytes = Report.getAllAsCsvFile(DateTime.parse(fileName)))
-        }
-        post {
-            val value: CsvValues = call.receive()
-            call.respond(HttpStatusCode.OK, Report.insert(value))
         }
     }
 }
