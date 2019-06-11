45cc7a9b0a76d03564ecef713a320f083b36ca5d
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2019-06-11 08:13:01 +0200

    release: 1.0.43

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 group = "no.nav.integrasjon"
-version = "1.0.43-SNAPSHOT"
+version = "1.0.43"
 
 val camel_version = "2.22.2"
 val ibm_mq_version = "9.1.1.0"
