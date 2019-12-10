875c1c78b288e9adb6c6fd2651f7dffdc70b9b35
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2019-12-10 08:17:32 +0100

    fix: use correct method for formatting byte size to human readable format

--- src/main/kotlin/no/nav/ehandel/kanal/common/extensions/Long.kt
+++ src/main/kotlin/no/nav/ehandel/kanal/common/extensions/Long.kt
@@ -1,12 +1,17 @@
 package no.nav.ehandel.kanal.common.extensions
 
-import kotlin.math.ln
-import kotlin.math.pow
+import kotlin.math.abs
 
 fun Long.humanReadableByteCount(): String {
-    val unit = 1000
-    if (this < unit) return "$this B"
-    val exp = (ln(this.toDouble()) / ln(unit.toDouble())).toInt()
-    val pre = "kMGTPE"[exp - 1]
-    return String.format("%.1f %sB", this / unit.toDouble().pow(exp.toDouble()), pre)
+    val s = if (this < 0) "-" else ""
+    var b = if (this == Long.MIN_VALUE) Long.MAX_VALUE else abs(this)
+    return when {
+        b < 1000L -> "$this B"
+        b < 999950L -> String.format("%s%.1f kB", s, b / 1e3)
+        1000.let { b /= it; b } < 999950L -> String.format("%s%.1f MB", s, b / 1e3)
+        1000.let { b /= it; b } < 999950L -> String.format("%s%.1f GB", s, b / 1e3)
+        1000.let { b /= it; b } < 999950L -> String.format("%s%.1f TB", s, b / 1e3)
+        1000.let { b /= it; b } < 999950L -> String.format("%s%.1f PB", s, b / 1e3)
+        else -> String.format("%s%.1f EB", s, b / 1e6)
+    }
 }
