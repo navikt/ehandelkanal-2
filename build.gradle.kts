983ff48d19624f8035daa5abd657fecf8e6c368c
rasmusvknav <rasmus.vedholm.krog@nav.no>
2025-09-02 09:49:07 +0200

    Remove old backend compiler argument from compileTestKotlin task

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ val kluent_version = "1.73"
 plugins {
     application
     kotlin("jvm") version "1.9.24"
-    id("org.jmailen.kotlinter") version "5.2.0"
+    //id("org.jmailen.kotlinter") version "5.2.0"
     id("com.github.ben-manes.versions") version "0.51.0"
     id("org.flywaydb.flyway") version "6.0.8"
     id("com.github.johnrengelman.shadow") version "7.1.2"
@@ -132,9 +132,6 @@ tasks {
         kotlinOptions {
             jvmTarget = "11"
         }
-        if (name == "compileTestKotlin") {
-        kotlinOptions.freeCompilerArgs += listOf("-Xuse-old-backend")
-    }
     }
     named("distZip")  { dependsOn("shadowJar") }
     named("distTar")  { dependsOn("shadowJar") }
