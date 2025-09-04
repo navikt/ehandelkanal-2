8f5753e3da84482d1c85f2b837c6e28280ef15e4
rasmusvknav <rasmus.vedholm.krog@nav.no>
2025-09-04 08:47:06 +0200

    Endret sequecne

--- src/main/resources/db/migration/postgresql/V1.3__add_report_sequence.sql
+++ src/main/resources/db/migration/postgresql/V1.3__add_report_sequence.sql
@@ -1,1 +1,7 @@
 SELECT pg_get_serial_sequence('report', 'id');
+
+ALTER TABLE report
+    ALTER COLUMN id SET DEFAULT nextval('report_id_seq');
+
+SELECT pg_get_serial_sequence('report', 'id');  -- usually 'report_id_seq'
+
